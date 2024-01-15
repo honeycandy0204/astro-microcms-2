a4696f5c657c392d2eca0af28bd8ed66c669c8e4
Honey Candy <rkomaki@gmail.com>
2024-01-15 16:20:07 +0900

    sdkインストール tsファイル作成

---
+++ src/library/microcms.ts
@@ -0,0 +1,33 @@
+import { createClient } from "microcms-js-sdk";
+const client = createClient({
+    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+//型定義
+export type Blog = {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    title: string;
+    content: string;
+};
+export type BlogResponse = {
+    totalCount: number;
+    offset: number;
+    limit: number;
+    contents: Blog[];
+};
+
+//API呼び出し
+export const getBlogs = async (queries?: any) => {
+    return await client.get<BlogResponse>({ endpoint: "blog", queries });
+};
+export const getBlogDetail = async (contentId: string, queries?: any) => {
+    return await client.getListDetail<Blog>({
+        endpoint: "blog",
+        contentId,
+        queries,
+    });
+};

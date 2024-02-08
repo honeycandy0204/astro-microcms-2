47650ecc48e6cc245aa5a96b07cf03c9c2210609
Honey Candy <rkomaki@gmail.com>
2024-02-08 12:40:32 +0900

    ページネーション導入直前のバックアップ

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -1,40 +1,41 @@
-import { createClient } from "microcms-js-sdk";
+import { createClient } from 'microcms-js-sdk';
 const client = createClient({
-    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
-    apiKey: import.meta.env.MICROCMS_API_KEY,
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 //型定義
 export type Blog = {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    revisedAt: string;
-    title: string;
-    content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  publishDate: string;
+  content: string;
 
-    // eyecatch追加
-    eyecatch?: {
-        url:string;
-    }
+  // eyecatch追加
+  eyecatch?: {
+    url: string;
+  };
 };
 export type BlogResponse = {
-    totalCount: number;
-    offset: number;
-    limit: number;
-    contents: Blog[];
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Blog[];
 };
 
 //API呼び出し、eyecatchパラメータ追加により修正
 export const getBlogs = async (queries?: { fields?: string[] }) => {
-    const fields = queries?.fields ? queries.fields.concat("eyecatch") : ["eyecatch"];
+  const fields = queries?.fields ? queries.fields.concat('eyecatch') : ['eyecatch'];
 
-    return await client.get<BlogResponse>({ endpoint: "blogs", queries:{ ...queries,fields }, });
+  return await client.get<BlogResponse>({ endpoint: 'blogs', queries: { ...queries, fields } });
 };
 export const getBlogDetail = async (contentId: string, queries?: any) => {
-    return await client.getListDetail<Blog>({
-        endpoint: "blogs",
-        contentId,
-        queries,
-    });
+  return await client.getListDetail<Blog>({
+    endpoint: 'blogs',
+    contentId,
+    queries,
+  });
 };
